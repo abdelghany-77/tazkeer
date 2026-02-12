4ce0d5d063ac734a1ee7cf99e7809ed34cd06acb
Mohamed Ahmed Abdelghany <11410120200423@stud.cu.edu.eg>
2026-02-12 19:12:59 +0200

    Refactor code structure for improved readability and maintainability

--- sw.js
+++ sw.js
@@ -125,5 +125,5 @@ self.addEventListener("push", function (event) {
 // Handle notification click
 self.addEventListener("notificationclick", function (event) {
   event.notification.close();
-  event.waitUntil(clients.openWindow("/"));
+  event.waitUntil(clients.openWindow("./"));
 });
