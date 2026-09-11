26d35b24c9e73bf9f7fadda2fe4bdf86b8949263
Mohamed Ahmed Abdelghany <11410120200423@stud.cu.edu.eg>
2026-09-11 11:52:46 +0300

    refactor: simplify QuranView navigation state and back behavior

--- src/components/QuranView.tsx
+++ src/components/QuranView.tsx
@@ -10,32 +10,13 @@ interface QuranViewProps {
 }
 
 export const QuranView: React.FC<QuranViewProps> = ({ initialPage, onClearInitialPage }) => {
-  const { khatmah, scope, setCurrentPage, setActiveSurahNumber, setScope, setWirdRange, setIsReaderOpen } = useQuran();
+  const { setCurrentPage, setActiveSurahNumber, setScope, setWirdRange, setIsReaderOpen } = useQuran();
   const [subView, setSubView] = useState<'index' | 'reader' | 'khatmah'>(() => {
     if (initialPage) {
       return 'reader';
     }
-    if (khatmah.isActive) {
-      return 'khatmah';
-    }
     return 'index';
   });
-
-  const [returnSubView, setReturnSubView] = useState<'index' | 'khatmah'>(() => {
-    if (khatmah.isActive) return 'khatmah';
-    return 'index';
-  });
-
-  const [hasAutoNavigated, setHasAutoNavigated] = useState(false);
-
-  // Sync subView if Khatmah state loads asynchronously
-  React.useEffect(() => {
-    if (!hasAutoNavigated && !initialPage && khatmah.isActive) {
-      setSubView('khatmah');
-      setReturnSubView('khatmah');
-      setHasAutoNavigated(true);
-    }
-  }, [khatmah.isActive, initialPage, hasAutoNavigated]);
 
   React.useEffect(() => {
     setIsReaderOpen(subView === 'reader');
@@ -46,18 +27,15 @@ export const QuranView: React.FC<QuranViewProps> = ({ initialPage, onClearInitia
   React.useEffect(() => {
     if (initialPage) {
       setCurrentPage(initialPage);
-      setWirdRange(null);
       if (initialPage === 293) {
         // Surah Al-Kahf is Surah 18
         setActiveSurahNumber(18);
         setScope('surah');
-      } else {
-        setScope('all');
       }
       setSubView('reader');
       if (onClearInitialPage) onClearInitialPage();
     }
-  }, [initialPage, setCurrentPage, setActiveSurahNumber, setScope, setWirdRange, onClearInitialPage]);
+  }, [initialPage, setCurrentPage, setActiveSurahNumber, setScope, onClearInitialPage]);
 
   const handleSelectPage = (page: number, surahNumber?: number) => {
     setCurrentPage(page);
@@ -68,7 +46,6 @@ export const QuranView: React.FC<QuranViewProps> = ({ initialPage, onClearInitia
     } else {
       setScope('all');
     }
-    setReturnSubView('index');
     setSubView('reader');
   };
 
@@ -77,22 +54,13 @@ export const QuranView: React.FC<QuranViewProps> = ({ initialPage, onClearInitia
     setCurrentPage(pageToOpen);
     setWirdRange({ startPage, endPage });
     setScope('wird');
-    setReturnSubView('khatmah');
     setSubView('reader');
-  };
-
-  const handleBackFromReader = () => {
-    if (scope === 'wird' || khatmah.isActive) {
-      setSubView('khatmah');
-    } else {
-      setSubView(returnSubView);
-    }
   };
 
   return (
     <div className="h-full w-full flex-1 overflow-hidden flex flex-col">
       {subView === 'reader' && (
-        <QuranReader onBackToIndex={handleBackFromReader} />
+        <QuranReader onBackToIndex={() => setSubView('index')} />
       )}
       {subView === 'index' && (
         <SurahList
